2a64a1b49513346c7104c428a74d4dbc7a608d1a
earth2travis <travis.mccutcheon@gmail.com>
2023-06-20 16:50:39 -0500

    Update home view and delete pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,9 @@
-import styled from "styled-components";
-
-import { H2, Link, ParMd, SingleColumnLayout } from "@daohaus/ui";
-import { HausAnimated } from "../components/HausAnimated";
-import { StyledRouterLink } from "../components/Layout";
-
-const LinkBox = styled.div`
-  display: flex;
-  width: 50%;
-  justify-content: space-between;
-`;
+import { H2, SingleColumnLayout } from '@daohaus/ui';
 
 export const Home = () => {
   return (
     <SingleColumnLayout>
-      <H2>DAOhaus is your haus</H2>
-      <HausAnimated />
-      <ParMd style={{ marginBottom: "2.4rem" }}>
-        Get started by editing src/pages/Home.tsx
-      </ParMd>
-      <LinkBox>
-        <Link href="https://github.com/HausDAO/monorepo">Github</Link>
-        <Link href="https://admin.daohaus.fun/">Admin</Link>
-        <StyledRouterLink to="/formtest">Example Form</StyledRouterLink>
-      </LinkBox>
+      <H2>A purpose-driven governance stack for your Arbitrum community.</H2>
     </SingleColumnLayout>
   );
 };
