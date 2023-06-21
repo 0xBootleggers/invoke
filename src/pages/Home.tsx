9024f7997bb98cbb7801f55ce4990c51493aa2e1
skuhlmann <samkuhlmann@gmail.com>
2023-06-21 16:56:44 +0100

    component styling tesst

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
-import { H2, SingleColumnLayout } from '@daohaus/ui';
+import { Button, H2, SingleColumnLayout } from "@daohaus/ui";
+import { StyledButton } from "../components/customDesigns/StyledButton";
 
 export const Home = () => {
   return (
     <SingleColumnLayout>
       <H2>A purpose-driven governance stack for your Arbitrum community.</H2>
+      <StyledButton className="poopin">POOPIN</StyledButton>
     </SingleColumnLayout>
   );
 };
