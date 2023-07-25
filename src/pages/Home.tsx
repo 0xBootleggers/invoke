4f9fbeb26e01e95fd67f99e05d7cadebee952ab6
skuhlmann <samkuhlmann@gmail.com>
2023-07-25 08:51:12 -0600

    sets up faqs on home and adds 2nd button link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import { Button, H2, SingleColumnLayout } from "@daohaus/ui";
-import { RouterLinkButton } from "../components/Layout";
+import { Button, H2, H4, Link, SingleColumnLayout } from "@daohaus/ui";
+import { RouterLinkButton, UnstyledUiLink } from "../components/Layout";
 import styled from "styled-components";
+import { FaqContent } from "../utils/content";
+import { Collapser } from "../formWizard";
 
 const ButtonLinks = styled.div`
   display: flex;
@@ -8,6 +10,12 @@ const ButtonLinks = styled.div`
   flex-wrap: wrap;
   justify-content: flex-start;
   gap: 3em;
+  width: 100%;
+`;
+
+const FaqContainer = styled.div`
+  margin-top: 15rem;
+  text-align: left;
   width: 100%;
 `;
 
@@ -19,7 +27,28 @@ export const Home = () => {
         <Button>
           <RouterLinkButton to="/invoke">Create</RouterLinkButton>
         </Button>
+        <Button color="secondary">
+          <UnstyledUiLink
+            href="https://docs.daohaus.club/"
+            showExternalIcon={false}
+          >
+            Docs
+          </UnstyledUiLink>
+        </Button>
       </ButtonLinks>
+
+      <FaqContainer>
+        <H4>FAQs</H4>
+        {FaqContent.map((faq) => {
+          return (
+            <Collapser
+              title={faq.title}
+              content={faq.content}
+              key={faq.title}
+            />
+          );
+        })}
+      </FaqContainer>
     </SingleColumnLayout>
   );
 };
