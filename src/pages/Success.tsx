11e6f26ba5d90e30792a5e38c23255cf3793ed31
skuhlmann <samkuhlmann@gmail.com>
2023-08-22 11:12:41 -0600

    adds confetti

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 import { Button, DataSm, H3, Link } from "@daohaus/ui";
 import { InvokeContainer } from "../components/Layout";
@@ -19,45 +21,54 @@ const LinkButton = styled(Link)`
 `;
 
 export const Success = () => {
+  const { width, height } = useWindowSize();
   const { daoId } = useParams();
 
   return (
-    <InvokeContainer>
-      <H3 style={{ marginBottom: "3rem" }}>Your DAO is live! 🎉</H3>
-      <DataSm style={{ marginBottom: "3rem" }}>
-        You have successfully summoned your DAO! So now you’re probably
-        wondering what to do next.
-      </DataSm>
+    <>
+      <Confetti
+        width={width}
+        height={1300}
+        numberOfPieces={6000}
+        initialVelocityX={6}
+      />
+      <InvokeContainer>
+        <H3 style={{ marginBottom: "3rem" }}>Your DAO is alive! 🎉</H3>
+        <DataSm style={{ marginBottom: "3rem" }}>
+          You have successfully summoned your DAO! So now you’re probably
+          wondering what to do next.
+        </DataSm>
 
-      <ButtonContainer>
-        <Button fullWidth>
-          <LinkButton
-            showExternalIcon={false}
-            href="https://docs.daohaus.club/"
-          >
-            View Docs
-          </LinkButton>
-        </Button>
-        <Button fullWidth>
-          <LinkButton
-            showExternalIcon={false}
-            href={`https://admin.daohaus.club/#/molochv3/${
-              import.meta.env.VITE_TARGET_CHAIN
-            }/${daoId}/settings`}
-          >
-            Advanced Settings
-          </LinkButton>
-        </Button>
-        <Button fullWidth>
-          <LinkButton
-            showExternalIcon={false}
-            href="https://admin.daohaus.club/"
-          >
-            Visit the Hub
-          </LinkButton>
-        </Button>
-      </ButtonContainer>
-    </InvokeContainer>
+        <ButtonContainer>
+          <Button fullWidth>
+            <LinkButton
+              showExternalIcon={false}
+              href="https://docs.daohaus.club/"
+            >
+              View Docs
+            </LinkButton>
+          </Button>
+          <Button fullWidth>
+            <LinkButton
+              showExternalIcon={false}
+              href={`https://admin.daohaus.club/#/molochv3/${
+                import.meta.env.VITE_TARGET_CHAIN
+              }/${daoId}/settings`}
+            >
+              Advanced Settings
+            </LinkButton>
+          </Button>
+          <Button fullWidth>
+            <LinkButton
+              showExternalIcon={false}
+              href="https://admin.daohaus.club/"
+            >
+              Visit the Hub
+            </LinkButton>
+          </Button>
+        </ButtonContainer>
+      </InvokeContainer>
+    </>
   );
 };
 
